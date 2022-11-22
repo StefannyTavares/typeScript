612a4f9970ed73d509b374cc53c94b9ccfa5addb
Stefanny Tavares <stefanny.tavares@hotmail.com>
2022-11-22 12:18:32 -0300

    Feat: Modified button

--- public/posts.js
+++ public/posts.js
@@ -1,1 +1,52 @@
-let descricao = "ola";
+var BoxPostList = /** @class */ (function () {
+    function BoxPostList() {
+        this.buttonListSelector = "#".concat(BoxPostList.boxId, ">button[type=button]");
+        this.init();
+    }
+    BoxPostList.prototype.init = function () {
+        var _this = this;
+        var buttonList = document.querySelector(this.buttonListSelector);
+        buttonList.addEventListener('click', function () {
+            _this.hiddenBox();
+            var boxForm = document.getElementById(BoxPostForm.boxId);
+            boxForm.removeAttribute('style');
+        });
+    };
+    BoxPostList.prototype.hiddenBox = function () {
+        var boxList = document.getElementById(BoxPostList.boxId);
+        boxList.style.display = 'none';
+    };
+    BoxPostList.prototype.showBox = function () {
+        var boxList = document.getElementById(BoxPostList.boxId);
+        boxList.removeAttribute('style');
+    };
+    BoxPostList.boxId = 'box-post-list';
+    return BoxPostList;
+}());
+var BoxPostForm = /** @class */ (function () {
+    function BoxPostForm() {
+        this.buttonFormSelector = "#".concat(BoxPostForm.boxId, ">button[type=button]");
+        this.init();
+    }
+    BoxPostForm.prototype.init = function () {
+        var _this = this;
+        var buttonForm = document.querySelector(this.buttonFormSelector);
+        buttonForm.addEventListener('click', function () {
+            _this.hiddenBox();
+            var boxList = document.getElementById(BoxPostList.boxId);
+            boxList.removeAttribute('style');
+        });
+    };
+    BoxPostForm.prototype.hiddenBox = function () {
+        var boxForm = document.getElementById(BoxPostForm.boxId);
+        boxForm.style.display = 'none';
+    };
+    BoxPostForm.prototype.showBox = function () {
+        var boxForm = document.getElementById(BoxPostForm.boxId);
+        boxForm.removeAttribute('style');
+    };
+    BoxPostForm.boxId = 'box-post-form';
+    return BoxPostForm;
+}());
+new BoxPostList();
+new BoxPostForm();
---
+++ src/posts.ts
@@ -0,0 +1,58 @@
+class BoxPostList {
+
+    static boxId = 'box-post-list';
+    private buttonListSelector = `#${BoxPostList.boxId}>button[type=button]`;
+
+    constructor(){
+        this.init();
+    }
+    private init(){
+        const buttonList = document.querySelector(this.buttonListSelector);
+        buttonList.addEventListener ('click',()=>{
+          this.hiddenBox()
+          
+          const boxForm = document.getElementById(BoxPostForm.boxId);
+          boxForm.removeAttribute('style')
+        });
+    }
+    hiddenBox(){
+        const boxList = document.getElementById(BoxPostList.boxId);
+        boxList.style.display = 'none';
+    }
+
+    showBox(){
+        const boxList = document.getElementById(BoxPostList.boxId);
+        boxList.removeAttribute('style');
+    }
+}
+
+class BoxPostForm {
+
+    static boxId = 'box-post-form';
+    private buttonFormSelector = `#${BoxPostForm.boxId}>button[type=button]`;
+
+    constructor(){
+        this.init();
+    }
+    private init(){
+        const buttonForm = document.querySelector(this.buttonFormSelector);
+        buttonForm.addEventListener ('click',()=>{
+          this.hiddenBox()
+          
+          const boxList = document.getElementById(BoxPostList.boxId);
+          boxList.removeAttribute('style')
+        });
+    }
+    hiddenBox(){
+        const boxForm = document.getElementById(BoxPostForm.boxId);
+        boxForm.style.display = 'none';
+    }
+
+    showBox(){
+        const boxForm = document.getElementById(BoxPostForm.boxId);
+        boxForm.removeAttribute('style');
+    }
+}
+
+new BoxPostList();
+new BoxPostForm();
